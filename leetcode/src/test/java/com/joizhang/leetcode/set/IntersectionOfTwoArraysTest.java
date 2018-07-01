aa1bfbf7d839b9ddeff5fb74fbc3c06d08efffdc
joizhang <zxlwhom@outlook.com>
2018-07-01 19:49:50 +0800

    leetcode

---
+++ leetcode/src/test/java/com/joizhang/leetcode/set/IntersectionOfTwoArraysTest.java
@@ -0,0 +1,19 @@
+package com.joizhang.leetcode.set;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class IntersectionOfTwoArraysTest {
+
+    @Test
+    public void intersection() {
+        IntersectionOfTwoArrays intersection = new IntersectionOfTwoArrays();
+        int[] nums1 = {1, 2, 2, 1};
+        int[] nums2 = {2, 2};
+        int[] res = intersection.intersection(nums1, nums2);
+        for (int num : res) {
+            System.out.print(num + " ");
+        }
+    }
+}

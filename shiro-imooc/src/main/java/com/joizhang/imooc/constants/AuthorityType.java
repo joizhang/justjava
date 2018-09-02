0b87f3051b30062c14bcc64e148afb5867d538be
joizhang <zxlwhom@outlook.com>
2018-09-02 22:27:16 +0800

    refactor: remove shiro-imooc

--- shiro-imooc/src/main/java/com/joizhang/imooc/constants/AuthorityType.java
+++
@@ -1,9 +0,0 @@
-package com.joizhang.imooc.constants;
-
-import java.io.Serializable;
-
-public enum AuthorityType implements Serializable {
-
-    School_Level_Admin, College_Level_Admin, Teacher, Expert, Admin
-
-}
--- shiro-imooc/src/main/resources/sql/scheml.sql
+++
@@ -1,9 +0,0 @@
-drop table if exists user;
-create table user (
-  username varchar(32) NOT NULL COMMENT '用户名',
-  password varchar(64) NOT NULL COMMENT '密码',
-  role varchar(32) NOT NULL COMMENT '角色',
-  authoritytype varchar(32) default null
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-INSERT INTO user (userName, passWord, role, authorityType) VALUES ('admin', '9cf3e758a497c6274bd066d0b2168432f8a34aad95f63a65677a9a56acec94a7', 'ADMIN', null);
